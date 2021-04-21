676d563338acf0b2fe88dd5f9a4dfd3e9e3f6647
Alejandro Zalazar <azalazar@veritran.com>
2021-04-21 19:06:06 -0300

    Fix SPM

--- Kuru.podspec
+++ Kuru.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'Kuru'
-  s.version          = '0.0.2'
+  s.version          = '0.0.3'
   s.summary          = 'Kuru is the easy way to manipulating ui data from a server in SwiftUI.'
   s.description      = <<-DESC
   Kuru is the easy way to manipulating ui data from a server in SwiftUI.
--- Package.swift
+++ Package.swift
@@ -17,15 +17,11 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Kuru",
-            dependencies: [],
-            path: "Sources"),
+            dependencies: []),
         .testTarget(
             name: "KuruTests",
-            dependencies: ["Kuru"],
-            path: "Tests"),
+            dependencies: ["Kuru"]),
     ]
 )
