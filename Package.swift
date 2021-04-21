9323a05750eada195d2e65c197bf984f83d80f67
Alejandro Zalazar <azalazar@veritran.com>
2021-04-21 18:44:51 -0300

    Adjust SPM sources

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Kuru",
+            path: "Sources",
             dependencies: []),
         .testTarget(
             name: "KuruTests",
